5e63bf3f130cf5d15eb95b64d6a9f2126d0bc700
Tony Bowden <tony@tmtm.com>
2021-08-24 17:48:44 +0300

    Skip the President from diffs

--- bin/diff.rb
+++ bin/diff.rb
@@ -3,5 +3,11 @@
 
 require 'every_politician_scraper/comparison'
 
+# Not listed on the site
+SKIP = [
+  ['---', 'Moon Jae-in', 'president of South Korea']
+].freeze
+
 diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+                                         .reject { |row| SKIP.include? row }
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
