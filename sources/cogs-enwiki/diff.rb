a149f99494a653711d275114286fd19d8a54d3d2
Tony Bowden <tony@tmtm.com>
2022-05-02 10:04:57 +0300

    update comparison approach

--- sources/cogs-enwiki/diff.rb
+++ sources/cogs-enwiki/diff.rb
@@ -3,5 +3,8 @@
 
 require 'every_politician_scraper/comparison'
 
-diff = EveryPoliticianScraper::NulllessComparison.new('wikidata.csv', 'scraped.csv').diff
+class Comparison < EveryPoliticianScraper::NulllessComparison
+end
+
+diff = Comparison.new('wikidata.csv', 'scraped.csv').diff
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
