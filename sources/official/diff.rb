414fb90a091fc562e8238efa6bd8f8b61baf1d93
Tony Bowden <tony@tmtm.com>
2022-05-02 09:54:38 +0300

    move official comparison to sources/

--- sources/official/diff.rb
+++ sources/official/diff.rb
@@ -8,6 +8,6 @@ SKIP = [
   ['---', 'Moon Jae-in', 'president of South Korea']
 ].freeze
 
-diff = EveryPoliticianScraper::NulllessComparison.new('data/wikidata.csv', 'data/official.csv').diff
+diff = EveryPoliticianScraper::NulllessComparison.new('wikidata.csv', 'scraped.csv').diff
                                          .reject { |row| SKIP.include? row }
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
