135fb9e63d749cde74e4d92ab2d898184bf30794
Tony Bowden <tony@tmtm.com>
2022-07-06 07:55:14 +0300

    move Attendees scraper into sources/

--- bin/compare-attendees.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-bundle exec ruby bin/scraper/cabinet-attendees.rb > data/cabinet-attendees.csv
-# No comparison yet: scraping just to track diffs
---
+++ sources/official-attendees/compare.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+bundle exec ruby scraper.rb > scraped.csv
+# No comparison yet: scraping just to track diffs
